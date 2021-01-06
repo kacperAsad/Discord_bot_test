772753581640481906a73df9115e125db95047d1
Kacper <58139949+kacperAsad@users.noreply.github.com>
2021-01-06 14:13:38 +0100

    Bot code add

--- bot.py
+++ bot.py
@@ -2,22 +2,17 @@ import discord
 import os
 from sys import argv
 
-print("WYPISYWANIE ARGUMENTÓW")
+
 for arg in argv:
-    print(arg)
-print("ZAKOŃCZONO")
+    if arg.startswith('-'):
+
+        TOKEN = arg.replace("-", " ", 1)
 
 
-# for arg in argv:
-#     if arg.startswith('-'):
-#
-#         TOKEN = arg.replace("-", " ", 1)
-#
-#
-# client = discord.Client()
-#
-# @client.event
-# async def on_ready():
-#     print(f'{client.user} has connected to Discord!')
-#
-# client.run(TOKEN)
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
