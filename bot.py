db37ab2a00a16676382233180aba8ef35988b20a
Kacper <58139949+kacperAsad@users.noreply.github.com>
2021-01-06 14:43:22 +0100

    Bot code add

--- bot.py
+++ bot.py
@@ -15,4 +15,26 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == 'dm':
+        await message.member.create_dm()
+        await message.dm_channel.send(
+        f'Hi, welcome to my Discord server!')
+        return
+
+
+    await message.channel.send(Byczq)
 client.run(TOKEN)
